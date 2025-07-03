5ab88b06e44d694bf7eb38c93c5d0391f97ebf80
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-07-03 21:19:26 +0530

    Day 11

---
+++ cybernaut/day11_1.py
@@ -0,0 +1,13 @@
+filename = "student_data.txt"
+
+num_students = int(input("Enter the number of students: "))
+
+with open(filename, "w") as file:
+    file.write("Name\t\tMarks\n")
+    file.write("-" * 20 + "\n")
+    for _ in range(num_students):
+        name = input("Enter student name: ")
+        marks = input("Enter marks: ")
+        file.write(f"{name}\t\t{marks}\n")
+
+print(f"\nData written to {filename} successfully.\n")
