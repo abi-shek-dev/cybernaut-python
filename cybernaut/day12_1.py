ac234d832210cd91747ab74c8a3ebca5d7ea248a
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-07-03 21:33:02 +0530

    Day 12

---
+++ cybernaut/day12_1.py
@@ -0,0 +1,6 @@
+import math
+
+num = float(input("Enter a number: "))
+print(f"Square Root: {math.sqrt(abs(num))}")
+print(f"Natural Log (base e): {math.log(abs(num))}")
+print(f"Absolute Value: {abs(num)}")
